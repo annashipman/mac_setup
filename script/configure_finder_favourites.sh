647526c1ee274b28614b201cc2dd2b4ff6cc6068
Anna Shipman <anna@annashipman.co.uk>
2018-02-17 14:47:57 +0000

    Configure the Finder favourites
    I want my home directory and I don't want All My Files or Documents to appear in the list in the Finder.
    
    This is copying a commit by @SteveMarshall: https://github.com/SteveMarshall/suit/commit/efa1ba.

---
+++ script/configure_finder_favourites.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+curl -s -O https://raw.githubusercontent.com/robperc/FinderSidebarEditor/master/FinderSidebarEditor.py
+
+python -c "
+from FinderSidebarEditor import FinderSidebar
+sidebar = FinderSidebar()
+sidebar.remove('All My Files')
+sidebar.remove('Documents')
+sidebar.add('$HOME')
+"
+
+rm -rf FinderSidebarEditor.py*
